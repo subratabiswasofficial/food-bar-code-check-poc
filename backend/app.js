12e27f5cc5cd7c3690e89363f9268b9c0fb882c0
Priyanshushaw1 <priyanshushaw447@gmail.com>
2025-12-24 23:49:55 +0530

    initial commit ; closes #1

---
+++ backend/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(fileUpload());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+//upload barcode image
+app.post("/upload/barcode", (req, res) => {
+  if (req.files != null && req.files.barcode != null) {
+    const filename = req.files.barcode.name;
+    req.files.barcode.mv("./uploads/" + filename, (err) => {
+      if (err != null) {
+        console.log(err);
+        return res.status(400).json({
+          message: "upload error",
+        });
+      }
+      const jobId = Date.now();
+      return res.status(200).json({ jobId });
+    });
+  }
+
+  return res.status(400).json({
+    message: "file missing",
+  });
+});
+
+//upload food label image
+app.post("/upload/food-label", (req, res) => {
+  if (!req.files || !req.files["food-label"]) {
+    return res.status(400).json({
+      success: false,
+      message: "Food label image is required",
+    });
+  }
+  const jobId = Date.now();
+  res.json({ jobId });
+});
+
+//job status
+app.get("/job/status/:jobId", (req, res) => {
+  res.json({
+    jobId: req.params.jobId,
+    status: true,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server started");
+});
